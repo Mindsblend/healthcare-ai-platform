e6ec340cb62e0b07e55225728a3c66912a057eb5
kian <kianebtekari0@gmail.com>
2025-11-30 18:54:31 +0330

    fix: refactor health investment section

--- src/components/domain/(interface)/HealthInvestmentSection.tsx
+++ src/components/domain/(interface)/HealthInvestmentSection.tsx
@@ -4,15 +4,15 @@ const HealthInvestmentSection = () => {
   return (
     <div className="bg-section flex w-full flex-col items-center justify-center">
       {/* Title */}
-      <h2 className="font-aria text-color-title-on-dark mt-16 mb-16 max-w-[730px] text-center text-[50px] leading-16 font-bold">
+      <h2 className="font-aria text-color-title-on-dark mt-16 mb-16 max-w-3xl text-center text-6xl font-bold">
         سرمایه‌ای که هیچ بانکی نمی‌تواند به شما بدهد
       </h2>
 
       {/* Cards Wrapper */}
 
-      <div className="mb-16 grid w-full max-w-[1200px] grid-cols-[1fr_1fr_475px] gap-8 px-8">
+      <div className="mb-16 grid w-full max-w-[1200px] grid-cols-1 lg:grid-cols-[475px_1fr_1fr] gap-8 px-8">
         {/* RIGHT COLUMN (tall card) */}
-        <div className="from-accent-purple to-accent-blue flex h-[497px] w-[475px] flex-col justify-center rounded-3xl bg-linear-to-r p-6">
+        <div className="from-accent-purple to-accent-blue flex flex-col justify-center rounded-3xl bg-linear-to-r p-6">
           {/* Icon Circle */}
           <div className="mb-6 flex">
             <div className="bg-section flex h-[70px] w-[70px] items-center justify-center rounded-full">
@@ -25,11 +25,11 @@ const HealthInvestmentSection = () => {
             </div>
           </div>
 
-          <h3 className="font-aria text-color-title-on-light mb-4 text-[30px] leading-5 font-semibold">
+          <h3 className="font-aria text-color-title-on-light mb-4 text-3xl font-semibold">
             سلامتی؛ بزرگ‌ترین سرمایه‌ زندگی
           </h3>
 
-          <div className="font-ray text-color-body-on-light max-w-[600px] text-[16px] leading-5 whitespace-pre-line">
+          <div className="font-ray text-color-body-on-light max-w-[600px] text-base whitespace-pre-line">
             {`سلامتی نه یک «هزینه»، بلکه بنیادی‌ترین «سرمایه‌گذاری» زندگی است.
             وقتی بدن شما انرژی پاک دارد، ذهن شما شفاف‌تر می‌شود. وقتی تغذیه شما سالم است، تصمیم‌هایتان عمیق‌تر و اثرگذارتر خواهند بود.
         
@@ -42,8 +42,8 @@ const HealthInvestmentSection = () => {
         {/* LEFT COLUMN (2 stacked) */}
         <div className="flex flex-col gap-6 md:col-span-2">
           {/* Top-left small card */}
-          <div className="bg-section-deep flex h-[199px] flex-col items-center justify-center rounded-3xl">
-            <h3 className="font-aria text-color-title-on-dark max-w-[493px] text-[30px] font-semibold">
+          <div className="bg-section-deep flex flex-col items-center justify-center rounded-3xl">
+            <h3 className="font-aria text-color-title-on-dark max-w-2xl lg:max-w-xl self-start lg:self-center p-12 lg:px-5 text-[30px] font-semibold">
               مطالعات نشان می‌دهد که
               <span className="text-accent-purple"> ۸۰٪</span> بیماری‌های مزمن
               با تغذیه و سبک زندگی سالم{' '}
@@ -52,7 +52,7 @@ const HealthInvestmentSection = () => {
           </div>
 
           {/* Bottom-left small card */}
-          <div className="bg-section-deep flex h-[270px] flex-col justify-center rounded-3xl p-12">
+          <div className="bg-section-deep flex flex-col justify-center rounded-3xl p-12">
             {/* Icon Circle */}
             <div className="mb-4 flex">
               <div className="bg-section flex h-[70px] w-[70px] items-center justify-center rounded-full">
@@ -65,10 +65,10 @@ const HealthInvestmentSection = () => {
               </div>
             </div>
 
-            <h3 className="font-aria text-color-title-on-dark text-[30px] leading-11 font-semibold">
+            <h3 className="font-aria text-color-title-on-dark text-3xl font-semibold">
               حیات در دستانت
             </h3>
-            <p className="font-ray text-color-body-on-dark text-col max-w-[536px] leading-5">
+            <p className="font-ray text-color-body-on-dark text-col max-w-[536px]">
               تحقیقات دانشگاه هاروارد نشان داده است که انتخاب یک رژیم غذایی
               متعادل می‌تواند امید به زندگی را بین ۱۰ تا ۱۴ سال افزایش دهد. این
               تنها به معنای سال‌های بیشتر نیست، بلکه به معنای سال‌هایی با کیفیت
