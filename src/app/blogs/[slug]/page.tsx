5a83ac53a03b90eb5efed8c8523a603aa08b52cc
kian <kianebtekari0@gmail.com>
2026-05-10 17:51:43 +0330

    feat: create the blog details page

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useParams } from 'next/navigation'
+import Image from 'next/image'
 import { useBlogBySlug } from '@/features/shop/hooks/blogs/useBlogsBySlug'
 import Link from 'next/link'
 
@@ -40,5 +41,46 @@ export default function ProductPage() {
     )
   }
 
-  return <div className="text-red-600">{blog.title}</div>
+  return (
+    <div className="container text-black">
+      <div className="relative aspect-square w-full sm:aspect-2/1 lg:aspect-21/9">
+        <Image
+          src={blog.image}
+          fill
+          alt="blogs image"
+          className="rounded-3xl object-cover object-center"
+          priority
+        />
+      </div>
+
+      <div className="mt-5 space-y-5 lg:mt-12 lg:space-y-8">
+        <h1 className="font-aria text-2xl font-extrabold sm:text-4xl xl:text-6xl">
+          {blog.title}
+        </h1>
+
+        <div
+          className="font-ray text-sm font-medium sm:text-sm xl:text-lg"
+          dangerouslySetInnerHTML={{ __html: blog.description }}
+        ></div>
+
+        <div className="flex shrink-0 items-center gap-2.5">
+          <Image
+            src={blog.authorImage || '/images/default-avatar.png'}
+            alt="writer image"
+            width={46}
+            height={46}
+            className="rounded-full"
+          />
+          <div>
+            <h1 className="font-aria text-color-title-on-light text-[16px] font-extrabold">
+              {blog.author}
+            </h1>
+            <p className="font-ray text-color-title-on-light text-[14px]">
+              نویسنده و پژوهشگر
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
