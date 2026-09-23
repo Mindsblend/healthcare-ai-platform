0a4c4550ecf5ca618dcb8fe0b0936f2aeb0a9c85
Kian Ebtekari <kianebtekari0@gmail.com>
2026-09-24 00:12:21 +0330

    updata robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', 'feed'],
       disallow: [
         '/api/',
         '/dashboard/',
@@ -19,7 +19,6 @@ export default function robots(): MetadataRoute.Robots {
         '/order/',
         '/payment/',
         '/auth/',
-        '/feed/',
       ],
     },
 
